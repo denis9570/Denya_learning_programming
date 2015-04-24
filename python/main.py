3b07da4d4d5d87b0eb6b953edec6188d67fd603e
Artur Klesun <arturklesun@gmail.com>
2015-04-24 12:34:04 +0300

    Hello, it is first commit

---
+++ python/main.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+print('app' 'le,' 'pe' 'ar')
+
+
+
+
+# print ("""\
+# apple
+#     pear
+#        pineapple
+# """)
+
+
+
+# print("\"Game of thrones\" best book ev er")
+
+
+# width = 20
+# height = 5 * 9
+# print(width * height)
+
+
+# number = int(input('Ведите умное число и мы возведём его в 3-ю степень. '))
+# print(number**3,"Вот так-то йопта!!!!") # Это число возведено в 3-ю степень
